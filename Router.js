d2d3942a2be3ab4f8005446d0c77115c396322b0
Carlos Muñoz <carlosmunyozguimera@gmail.com>
2019-07-09 14:58:57 +0200

    website base

--- Router.js
+++ Router.js
@@ -1,1 +1,38 @@
-'use strict'
+'use strict';
+
+function Router() {
+  this.page = null;
+}
+
+Router.prototype.buildDom = function(url, parentElement) {
+  // if(url === '/'){
+  //   this.generateLandingPage();
+  // } else if( url === '/movies'){
+  //   this.generateMoviesPage();
+  // }
+  switch(url){
+    case '/':
+      this.generateLandingPage(parentElement);
+      break;
+    case '/movies':
+      this.generateMoviesPage(parentElement);
+      break;
+    default:
+      this.generateLandingPage(parentElement);
+  }
+} 
+
+Router.prototype.generateLandingPage = function(parentElement) {
+  this.page = new LandingPage(parentElement);
+  this.page.generate();
+}
+
+Router.prototype.generateMoviesPage = function(parentElement) {
+  this.page = new MoviesPage(parentElement);
+  this.page.generate();
+}
+
+
+// creamos una variable aquí dentro para que solo creemos una instancia de Router. Si lo hacemos en app.js
+// crearemos cada vez que llamemos a Router una instancia nueva en lugar de utilizar la misma
+var routerInstance = new Router();
